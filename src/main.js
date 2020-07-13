79a547096e7f68589325c4c7145e9088bc640cd1
Gabriel Krieshok <gabriel.krieshok@gmail.com>
2020-07-13 19:04:32 -0400

    Upgrade

--- src/main.js
+++ src/main.js
@@ -2,22 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import Buefy from 'buefy'
 import Vuelidate from 'vuelidate'
-import SocialSharing from 'vue-social-sharing'
 
-Vue.use(SocialSharing)
 Vue.use(Vuelidate)
 Vue.use(Buefy, {
   defaultIconPack: 'fa'
 })
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
+  render: h => h(App),
+}).$mount('#app')
