a49b2b1ad95d73b7ec3f973c1e6dacc3844dd3dd
Gabriel Krieshok <gabriel.krieshok@gmail.com>
2020-10-19 23:12:00 -0400

    Updates

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import Buefy from 'buefy'
 import Vuelidate from 'vuelidate'
+import VueAnalytics from 'vue-analytics';
+
 
 Vue.use(Vuelidate)
+
+Vue.use(VueAnalytics, {
+  id: 'UA-111965343-1'
+})
+
 Vue.use(Buefy, {
   defaultIconPack: 'fa'
 })
